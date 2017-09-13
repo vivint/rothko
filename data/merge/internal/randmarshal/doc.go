90cf4be105f1c02ee6d555d1b572c34d890b4a6a
Jeff Wendling <jeff@spacemonkey.com>
2017-09-13 13:52:52 -0600

    document randmarshal

--- data/merge/internal/randmarshal/doc.go
+++ data/merge/internal/randmarshal/doc.go
@@ -2,4 +2,5 @@
 
 //go:generate ./regen.sh
 
+// package randmarshal implements protobuf marshalling for gopkg.in/spacemonkeygo/random.v1
 package randmarshal // import "github.com/spacemonkeygo/rothko/data/merge/internal/randmarshal"
