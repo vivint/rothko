3519a341430b598129f6a83bb8eb64fcf5255b90
Jeff Wendling <jeff@spacemonkey.com>
2018-02-06 16:04:06 -0700

    ui: factor query bar into its own module

--- ui/src/Main.elm
+++ ui/src/Main.elm
@@ -2,16 +2,10 @@ module Main exposing (..)
 
 import Api
 import Graph
+import QueryBar
 import Html exposing (Html, text)
 import Html.Attributes as Attr
-import Html.Events as Ev
-import Window
 import Http
-import Autocomplete as Auto
-import Keyboard
-import Task
-import Time
-import Process
 
 
 -- MAIN
@@ -36,17 +30,11 @@ type alias Flags =
 
 type Msg
     = NoOp
-    | SetAutoState Auto.State
-    | Select
     | DrawStarting
     | DrawDone Graph.Result
-    | Render
     | RenderResponse (Result Http.Error String)
-    | SetQueryText String
-    | QueryTimer String
-    | Query String
-    | QueryResponse (Result Http.Error (List String))
-    | KeyPress Keyboard.KeyCode
+    | QueryBarMsg QueryBar.Msg
+    | Select String
 
 
 
@@ -60,41 +48,18 @@ type GraphState
     | Failed String
 
 
-type QueryResult
-    = QRNothing
-    | QRWaiting
-    | QRJust ( List String, Maybe Auto.State )
-
-
-queryResult_new : List String -> QueryResult
-queryResult_new results =
-    QRJust ( results, Just Auto.new )
-
-
-queryResult_mapAutoState : (Auto.State -> Auto.State) -> QueryResult -> QueryResult
-queryResult_mapAutoState fn queryResult =
-    case queryResult of
-        QRJust ( results, Just state ) ->
-            QRJust ( results, Just (fn state) )
-
-        _ ->
-            queryResult
-
-
-queryResult_toMaybe : QueryResult -> Maybe ( List String, Maybe Auto.State )
-queryResult_toMaybe queryResult =
-    case queryResult of
-        QRJust ( results, state ) ->
-            Just ( results, state )
-
-        _ ->
-            Nothing
-
-
 type alias Model =
     { graphState : GraphState
-    , queryResult : QueryResult
-    , queryText : String
+    , queryBar : QueryBar.Model
+    }
+
+
+queryBarConfig : QueryBar.Config Model Msg
+queryBarConfig =
+    { get = .queryBar
+    , set = \model queryBar -> { model | queryBar = queryBar }
+    , wrap = QueryBarMsg
+    , select = Select
     }
 
 
@@ -105,8 +70,7 @@ type alias Model =
 init : Flags -> ( Model, Cmd Msg )
 init flags =
     ( { graphState = Stale
-      , queryResult = QRNothing
-      , queryText = ""
+      , queryBar = QueryBar.new
       }
     , Cmd.none
     )
@@ -127,19 +91,6 @@ update msg model =
         NoOp ->
             ( model, Cmd.none )
 
-        SetAutoState autoState ->
-            ( { model
-                | queryResult =
-                    queryResult_mapAutoState
-                        (always autoState)
-                        model.queryResult
-              }
-            , Cmd.none
-            )
-
-        Select ->
-            update_select model
-
         DrawStarting ->
             ( { model | graphState = Drawing }, Cmd.none )
 
@@ -151,12 +102,6 @@ update msg model =
             , Cmd.none
             )
 
-        Render ->
-            Api.renderRequest model.queryText
-                |> Api.render
-                |> Http.send RenderResponse
-                |> withModel model
-
         RenderResponse response ->
             case response of
                 Ok result ->
@@ -165,114 +110,15 @@ update msg model =
                 Err error ->
                     Debug.log (toString error) ( model, Cmd.none )
 
-        SetQueryText queryText ->
-            case queryText of
-                "" ->
-                    ( { model | queryText = queryText, queryResult = QRNothing }
-                    , Cmd.none
-                    )
-
-                _ ->
-                    ( { model
-                        | queryText = queryText
-                        , queryResult = QRWaiting
-                      }
-                    , Process.sleep (250 * Time.millisecond)
-                        |> Task.perform (always (QueryTimer queryText))
-                    )
-
-        QueryTimer queryText ->
+        Select value ->
             ( model
-            , if queryText == model.queryText then
-                utils_sendMessage <| Query queryText
-              else
-                Cmd.none
+            , Api.renderRequest value
+                |> Api.render
+                |> Http.send RenderResponse
             )
 
-        Query query ->
-            Api.queryRequest query
-                |> Api.query
-                |> Http.send QueryResponse
-                |> withModel model
-
-        QueryResponse response ->
-            ( case response of
-                Ok results ->
-                    { model | queryResult = queryResult_new results }
-
-                _ ->
-                    model
-            , Cmd.none
-            )
-
-        KeyPress keyCode ->
-            update_keyCode model keyCode
-
-
-update_keyCode : Model -> Keyboard.KeyCode -> ( Model, Cmd Msg )
-update_keyCode model keyCode =
-    let
-        length =
-            model.queryResult
-                |> queryResult_toMaybe
-                |> Maybe.map Tuple.first
-                |> Maybe.map List.length
-                |> Maybe.withDefault 0
-    in
-        case keyCode of
-            -- UP
-            38 ->
-                ( { model
-                    | queryResult =
-                        queryResult_mapAutoState
-                            Auto.upPress
-                            model.queryResult
-                  }
-                , Cmd.none
-                )
-
-            -- DOWN
-            40 ->
-                ( { model
-                    | queryResult =
-                        queryResult_mapAutoState
-                            (Auto.downPress length)
-                            model.queryResult
-                  }
-                , Cmd.none
-                )
-
-            -- ENTER
-            13 ->
-                update_select model
-
-            -- ESCAPE
-            27 ->
-                ( { model | queryResult = QRNothing }, Cmd.none )
-
-            _ ->
-                ( model, Cmd.none )
-
-
-update_select : Model -> ( Model, Cmd Msg )
-update_select model =
-    ( case model.queryResult of
-        QRJust ( results, Just autoState ) ->
-            let
-                setSelection selected =
-                    { model
-                        | queryText = selected
-                        , queryResult = QRNothing
-                    }
-            in
-                Auto.selected autoState results
-                    |> Maybe.map setSelection
-                    |> Maybe.withDefault model
-
-        _ ->
-            model
-    , utils_sendMessage Render
-    )
+        QueryBarMsg msg ->
+            QueryBar.update queryBarConfig model msg
 
 
 
@@ -284,20 +130,12 @@ subscriptions model =
     Sub.batch
         [ Graph.starting DrawStarting
         , Graph.done DrawDone
-        , Keyboard.downs KeyPress
+        , QueryBar.subscriptions queryBarConfig model
         ]
 
 
 
 -- VIEW
-
-
-config : Auto.Config Msg
-config =
-    Auto.config
-        { toMsg = SetAutoState
-        , select = Select
-        }
 
 
 view : Model -> Html Msg
@@ -356,67 +194,4 @@ view_header =
 
 view_query : Model -> Html Msg
 view_query model =
-    let
-        attrs =
-            [ Attr.value model.queryText
-            , Attr.id "query"
-            , Attr.style [ ( "width", "100%" ) ]
-            , Ev.onInput SetQueryText
-            ]
-
-        autocomplete =
-            case model.queryResult of
-                QRJust ( results, Just autoState ) ->
-                    Just (Auto.view config autoState results)
-
-                _ ->
-                    Nothing
-    in
-        []
-            |> utils_add (Html.input attrs [])
-            |> utils_maybeAdd autocomplete
-            |> view_fullRow
-
-
-
--- UTILS
-
-
-utils_add : a -> List a -> List a
-utils_add val list =
-    list ++ [ val ]
-
-
-utils_maybeAdd : Maybe a -> List a -> List a
-utils_maybeAdd val list =
-    case val of
-        Nothing ->
-            list
-
-        Just val ->
-            utils_add val list
-
-
-utils_when : Bool -> a -> Maybe a
-utils_when cond val =
-    if cond then
-        Just val
-    else
-        Nothing
-
-
-utils_sendMessage : msg -> Cmd msg
-utils_sendMessage m =
-    Task.perform (\_ -> m) (Task.succeed ())
-
-
-utils_titleCase : String -> String
-utils_titleCase val =
-    let
-        upperWord word =
-            String.toUpper (String.left 1 word) ++ (String.dropLeft 1 word)
-    in
-        val
-            |> String.words
-            |> List.map upperWord
-            |> String.join " "
+    view_fullRow [ QueryBar.view queryBarConfig model.queryBar ]
---
+++ ui/src/QueryBar.elm
@@ -0,0 +1,345 @@
+module QueryBar
+    exposing
+        ( Model
+        , new
+        , Msg
+        , Config
+        , subscriptions
+        , update
+        , view
+        )
+
+import Api
+import Task
+import Html exposing (Html)
+import Html.Attributes as Attr
+import Html.Events as Ev
+import Keyboard
+import Process
+import Time
+import Http
+
+
+-- MODEL
+
+
+type Query
+    = None
+    | Got (Maybe Int) (List String)
+
+
+queryToMaybe : Query -> Maybe ( Maybe Int, List String )
+queryToMaybe query =
+    case query of
+        None ->
+            Nothing
+
+        Got highlight completions ->
+            Just ( highlight, completions )
+
+
+type Model
+    = Model
+        { value : String
+        , query : Query
+        }
+
+
+new : Model
+new =
+    Model
+        { value = ""
+        , query = None
+        }
+
+
+
+-- MSG
+
+
+type Msg
+    = SetValue String
+    | Timer String
+    | QueryResponse String (Result Http.Error (List String))
+    | Dismiss
+    | Select String
+    | KeyDown Keyboard.KeyCode
+    | MouseOver Int
+    | MouseLeave Int
+
+
+
+-- CONFIG
+
+
+type alias Config model msg =
+    { get : model -> Model
+    , set : model -> Model -> model
+    , wrap : Msg -> msg
+    , select : String -> msg
+    }
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Config model msg -> model -> Sub msg
+subscriptions config model =
+    doSubscriptions (config.get model)
+        |> Sub.map config.wrap
+
+
+doSubscriptions : Model -> Sub Msg
+doSubscriptions (Model model) =
+    Keyboard.downs KeyDown
+
+
+
+-- UPDATE
+
+
+update : Config model msg -> model -> Msg -> ( model, Cmd msg )
+update config model msg =
+    let
+        ( newModel, cmd, parentCmd ) =
+            doUpdate config (config.get model) msg
+    in
+        ( config.set model newModel
+        , Cmd.batch
+            [ Cmd.map config.wrap cmd
+            , parentCmd
+            ]
+        )
+
+
+doUpdate : Config model msg -> Model -> Msg -> ( Model, Cmd Msg, Cmd msg )
+doUpdate config (Model model) msg =
+    case msg of
+        SetValue value ->
+            ( Model { model | value = value, query = None }
+            , Process.sleep (250 * Time.millisecond)
+                |> Task.perform (always (Timer value))
+                |> when (value /= "")
+                |> Maybe.withDefault Cmd.none
+            , Cmd.none
+            )
+
+        Timer value ->
+            ( Model model
+            , Api.queryRequest value
+                |> Api.query
+                |> Http.send (QueryResponse value)
+                |> when (value == model.value)
+                |> Maybe.withDefault Cmd.none
+            , Cmd.none
+            )
+
+        QueryResponse value response ->
+            ( Result.toMaybe response
+                |> Maybe.andThen (when (value == model.value))
+                |> Maybe.map (\r -> { model | query = Got Nothing r })
+                |> Maybe.withDefault model
+                |> Model
+            , Cmd.none
+            , Cmd.none
+            )
+
+        Dismiss ->
+            ( Model { model | query = None }
+            , Cmd.none
+            , Cmd.none
+            )
+
+        Select value ->
+            ( Model { model | value = value, query = None }
+            , Cmd.none
+            , sendMessage (config.select value)
+            )
+
+        KeyDown keyCode ->
+            case model.query of
+                Got highlight completions ->
+                    let
+                        ( newHighlight, cmd ) =
+                            updateKeyCode highlight completions keyCode
+                    in
+                        ( Model { model | query = Got newHighlight completions }
+                        , cmd
+                        , Cmd.none
+                        )
+
+                None ->
+                    ( Model model
+                    , Cmd.none
+                    , Cmd.none
+                    )
+
+        MouseOver index ->
+            case model.query of
+                Got _ completions ->
+                    ( Model { model | query = Got (Just index) completions }
+                    , Cmd.none
+                    , Cmd.none
+                    )
+
+                _ ->
+                    ( Model model
+                    , Cmd.none
+                    , Cmd.none
+                    )
+
+        MouseLeave index ->
+            case model.query of
+                Got (Just current) completions ->
+                    if current == index then
+                        ( Model { model | query = Got Nothing completions }
+                        , Cmd.none
+                        , Cmd.none
+                        )
+                    else
+                        ( Model model
+                        , Cmd.none
+                        , Cmd.none
+                        )
+
+                _ ->
+                    ( Model model
+                    , Cmd.none
+                    , Cmd.none
+                    )
+
+
+updateKeyCode : Maybe Int -> List String -> Keyboard.KeyCode -> ( Maybe Int, Cmd Msg )
+updateKeyCode highlight completions keyCode =
+    case keyCode of
+        -- UP
+        38 ->
+            case highlight of
+                Nothing ->
+                    ( Nothing, Cmd.none )
+
+                Just val ->
+                    ( Just <| max (val - 1) 0, Cmd.none )
+
+        -- DOWN
+        40 ->
+            let
+                length =
+                    List.length completions
+            in
+                case ( length, highlight |> Maybe.withDefault -1 ) of
+                    ( 0, _ ) ->
+                        ( Nothing, Cmd.none )
+
+                    ( _, val ) ->
+                        ( Just <| min (val + 1) (length - 1), Cmd.none )
+
+        -- ENTER
+        13 ->
+            let
+                select completions index =
+                    (List.drop index >> List.head) completions
+            in
+                case highlight |> Maybe.andThen (select completions) of
+                    Just value ->
+                        ( highlight, sendMessage <| Select value )
+
+                    Nothing ->
+                        ( highlight, Cmd.none )
+
+        -- ESCAPE
+        27 ->
+            ( highlight, sendMessage Dismiss )
+
+        _ ->
+            ( highlight, Cmd.none )
+
+
+
+-- VIEW
+
+
+view : Config model msg -> Model -> Html msg
+view config model =
+    doView model
+        |> Html.map config.wrap
+
+
+doView : Model -> Html Msg
+doView (Model model) =
+    let
+        input =
+            [ Html.input
+                [ Attr.value model.value
+                , Attr.style [ ( "width", "100%" ) ]
+                , Ev.onInput SetValue
+                ]
+                []
+            ]
+
+        autocomplete =
+            case model.query of
+                Got highlight completions ->
+                    [ viewAutocomplete highlight completions ]
+
+                _ ->
+                    []
+
+        children =
+            []
+                ++ input
+                ++ autocomplete
+    in
+        Html.div [] children
+
+
+viewAutocomplete : Maybe Int -> List String -> Html Msg
+viewAutocomplete highlight completions =
+    Html.ul [ Attr.class "auto-menu" ] (viewCompletions highlight completions)
+
+
+viewCompletions : Maybe Int -> List String -> List (Html Msg)
+viewCompletions highlight completions =
+    List.indexedMap (viewCompletion highlight) completions
+
+
+viewCompletion : Maybe Int -> Int -> String -> Html Msg
+viewCompletion highlight index completion =
+    let
+        activeAttrs =
+            if Just index == highlight then
+                [ Attr.class "auto-menu-item-active" ]
+            else
+                []
+
+        attrs =
+            [ Attr.class "auto-menu-item"
+            , Ev.onMouseOver (MouseOver index)
+            , Ev.onMouseLeave (MouseLeave index)
+            , Ev.onClick (Select completion)
+            ]
+                ++ activeAttrs
+    in
+        Html.li attrs
+            [ Html.div
+                [ Attr.class "auto-menu-item-wrapper" ]
+                [ Html.text completion ]
+            ]
+
+
+
+-- UTILS
+
+
+sendMessage : msg -> Cmd msg
+sendMessage msg =
+    Task.perform (always msg) (Task.succeed ())
+
+
+when : Bool -> a -> Maybe a
+when cond val =
+    if cond then
+        Just val
+    else
+        Nothing
